5ccc0eedc86c2abefdbfa84255239ed02919707c
Eyal Sorek <eyalso@wix.com>
2015-06-24 13:04:55 +0300

    swtich to using poke file and turned off add_to_cron to false by default

--- attributes/poke.rb
+++ attributes/poke.rb
@@ -1,6 +1,3 @@
-default['mysqler'][:poke][:add_to_cron] = true
+default['mysqler'][:poke][:add_to_cron] = false
 default['mysqler'][:poke][:cron_hour] = "*"
 default['mysqler'][:poke][:cron_minute] = 5
-default['mysqler'][:poke][:poke_user] = "poke"
-default['mysqler'][:poke][:poke_db] = "test"
-default['mysqler'][:poke][:poke_table] = "poke"
